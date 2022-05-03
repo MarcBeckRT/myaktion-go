1d90b2c1ad1292057ed795490c304adfeb95bf75
MarcBeckRT <marc_stefan.beck@student.reutlingen-university.de>
2022-05-03 14:29:47 +0200

    push

--- src/myaktion/main.go
+++ src/myaktion/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/MarcBeckRT/myaktion-go/src/myaktion/handler"
+)
+
+func main() {
+	log.Println("Starting My-Aktion API server")
+	router := mux.NewRouter()
+	router.HandleFunc("/health", handler.Health).Methodes("GET")
+	if err := http.ListenAndServe(":8000", router); err != nil {
+		log.Fatal(err)
+	}
 }
