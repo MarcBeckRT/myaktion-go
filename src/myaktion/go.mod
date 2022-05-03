144b83eeb1f045e9ccdc8ddfd01ce49fd8ce52e8
MarcBeckRT <marc_stefan.beck@student.reutlingen-university.de>
2022-05-03 14:37:15 +0200

    not empty

--- src/myaktion/go.mod
+++ src/myaktion/go.mod
@@ -1,3 +1,5 @@
 module github.com/MarcBeckRT/myaktion-go/src/myaktion
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- src/myaktion/main.go
+++ src/myaktion/main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 
 	"github.com/MarcBeckRT/myaktion-go/src/myaktion/handler"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Println("Starting My-Aktion API server")
 	router := mux.NewRouter()
-	router.HandleFunc("/health", handler.Health).Methodes("GET")
+	router.HandleFunc("/health", handler.Health).Methods("GET")
 	if err := http.ListenAndServe(":8000", router); err != nil {
 		log.Fatal(err)
 	}
