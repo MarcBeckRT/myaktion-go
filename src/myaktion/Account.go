60c63eb7df5cd4d118497d3e7fcfe9eae6d5d96f
MarcBeckRT <marc_stefan.beck@student.reutlingen-university.de>
2022-05-03 13:52:17 +0200

    structs

---
+++ src/myaktion/Account.go
@@ -0,0 +1,7 @@
+package models
+
+type Account struct {
+	iban       string
+	name       string
+	nameOfBank string
+}
---
+++ src/myaktion/Campaign.go
@@ -0,0 +1,7 @@
+package models
+
+type Campaign struct {
+	name            string
+	donationMinimum float64
+	targetAmount    float64
+}
